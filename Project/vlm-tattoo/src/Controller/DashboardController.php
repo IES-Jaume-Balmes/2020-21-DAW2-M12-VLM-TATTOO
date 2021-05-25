e3b79e60bac077830fc4990a9024b26178c22e89
pgispert19 <pgispert19@jaumebalmes.net>
2021-05-25 18:00:36 +0200

    Estils Registre

--- Project/vlm-tattoo/src/Controller/DashboardController.php
+++ Project/vlm-tattoo/src/Controller/DashboardController.php
@@ -13,9 +13,6 @@ class DashboardController extends AbstractController
     #[Route('/', name: 'dashboard')]
     public function index(Request $request): Response
     {
-<<<<<<< HEAD
-
-=======
         //var_dump($request->request->get('form'));
         //$var = $request->query->get('')
         //var_dump($request->query->get('descripcion'));
@@ -24,6 +21,5 @@ class DashboardController extends AbstractController
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'Bienvenido a Dashboard',
         ]);
->>>>>>> 715c9d5090e0259de3a8ab09834ed76150c93f8b
     }
 }
