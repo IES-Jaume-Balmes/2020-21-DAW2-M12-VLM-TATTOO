64da0c6058cbc134f239025e6e44b26ca2098e21
david.janer <djaner20@jaumebalmes.net>
2021-06-01 19:42:42 +0200

    hugo

--- Project/vlm-tattoo/var/cache/dev/twig/81/8162a0c028c6e6ee48940c057dd2f364a5fc8cbb4acc88c413a3fb255b2f1744.php
+++ Project/vlm-tattoo/var/cache/dev/twig/81/8162a0c028c6e6ee48940c057dd2f364a5fc8cbb4acc88c413a3fb255b2f1744.php
@@ -85,57 +85,56 @@ class __TwigTemplate_057d9ab29b34f8f15f1f43ce24514f95db2360a94524630d564cd17bbbc
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
 
         // line 6
-        echo "<style>
-    .example-wrapper { margin: 1em auto; max-width: 800px; width: 95%; font: 18px/1.5 sans-serif; }
-    .example-wrapper code { background: #F5F5F5; padding: 2px 6px; }
-</style>
+        echo "    <style>
+        .example-wrapper {
+            margin: 1em auto;
+            max-width: 800px;
+            width: 95%;
+            font: 18px/1.5 sans-serif;
+        }
+        .example-wrapper code {
+            background: #F5F5F5;
+            padding: 2px 6px;
+        }
+    </style>
 
-<div class=\"example-wrapper\">
-    <h1>Pagament y cita correcte</h1>
-    <div class=\"card-body\">
-        <form method=\"post\" class=\"form\" autocomplete=\"off\" role=\"form\">
-
-            <div class=\"form-group\">
-                ";
-        // line 17
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["reserva"]) || array_key_exists("reserva", $context) ? $context["reserva"] : (function () { throw new RuntimeError('Variable "reserva" does not exist.', 17, $this->source); })()), "talla", [], "any", false, false, false, 17), "html", null, true);
-        echo "
-            </div>
-            <div class=\"form-group\">
-                ";
-        // line 20
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["reserva"]) || array_key_exists("reserva", $context) ? $context["reserva"] : (function () { throw new RuntimeError('Variable "reserva" does not exist.', 20, $this->source); })()), "descripcion", [], "any", false, false, false, 20), "html", null, true);
-        echo "
-            </div>
-            <div class=\"form-group\">
-                ";
-        // line 23
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["reserva"]) || array_key_exists("reserva", $context) ? $context["reserva"] : (function () { throw new RuntimeError('Variable "reserva" does not exist.', 23, $this->source); })()), "deposito", [], "any", false, false, false, 23), "html", null, true);
-        echo "
-            </div>
-            <div class=\"form-group\">
-                ";
-        // line 26
-        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, (isset($context["reserva"]) || array_key_exists("reserva", $context) ? $context["reserva"] : (function () { throw new RuntimeError('Variable "reserva" does not exist.', 26, $this->source); })()), "cliente", [], "any", false, false, false, 26), "nombre", [], "any", false, false, false, 26), "html", null, true);
-        echo "
-            </div>
-            <div class=\"form-group\">
-                ";
-        // line 29
-        echo twig_escape_filter($this->env, twig_date_format_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["reserva"]) || array_key_exists("reserva", $context) ? $context["reserva"] : (function () { throw new RuntimeError('Variable "reserva" does not exist.', 29, $this->source); })()), "fechaInicio", [], "any", false, false, false, 29), "d/m/Y H"), "html", null, true);
-        echo "h
-            </div>
-
-        </form>
+    <div class=\"container py-3\">
+    <div class=\"row\">
+    <div class=\"col-md-12\">
+    <div class=\"row justify-content-center\">
+    <div class=\"col-md-6\">
+    <div class=\"card card-outline-secodary\">
+        <div class=\"card-header\">
+            <h3 class=\"mb-0\">Pago y cita correctos</h3>
+        </div>
+        <div class=\"card-body\">
+            <ul class=\"list-group list-group-flush\">
+                <li class=\"list-group-item\"><strong>Talla: </strong><br>";
+        // line 30
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["reserva"]) || array_key_exists("reserva", $context) ? $context["reserva"] : (function () { throw new RuntimeError('Variable "reserva" does not exist.', 30, $this->source); })()), "talla", [], "any", false, false, false, 30), "html", null, true);
+        echo "</li>
+                <li class=\"list-group-item\"><strong>Descripción: </strong><br>";
+        // line 31
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["reserva"]) || array_key_exists("reserva", $context) ? $context["reserva"] : (function () { throw new RuntimeError('Variable "reserva" does not exist.', 31, $this->source); })()), "descripcion", [], "any", false, false, false, 31), "html", null, true);
+        echo "</li>
+                <li class=\"list-group-item\"><strong>Depósito: </strong><br>";
+        // line 32
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["reserva"]) || array_key_exists("reserva", $context) ? $context["reserva"] : (function () { throw new RuntimeError('Variable "reserva" does not exist.', 32, $this->source); })()), "deposito", [], "any", false, false, false, 32), "html", null, true);
+        echo "</li>
+                <li class=\"list-group-item\"><strong>Nombre: </strong><br>";
+        // line 33
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, (isset($context["reserva"]) || array_key_exists("reserva", $context) ? $context["reserva"] : (function () { throw new RuntimeError('Variable "reserva" does not exist.', 33, $this->source); })()), "cliente", [], "any", false, false, false, 33), "apellidos", [], "any", false, false, false, 33), "html", null, true);
+        echo ", ";
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, (isset($context["reserva"]) || array_key_exists("reserva", $context) ? $context["reserva"] : (function () { throw new RuntimeError('Variable "reserva" does not exist.', 33, $this->source); })()), "cliente", [], "any", false, false, false, 33), "nombre", [], "any", false, false, false, 33), "html", null, true);
+        echo "</li>
+                <li class=\"list-group-item\"><strong>Nombre de la Imagen: </strong><br>";
+        // line 34
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, (isset($context["reserva"]) || array_key_exists("reserva", $context) ? $context["reserva"] : (function () { throw new RuntimeError('Variable "reserva" does not exist.', 34, $this->source); })()), "imagen", [], "any", false, false, false, 34), "html", null, true);
+        echo "</li>
+            </ul>
+            <a href=\"/\" role=\"button\" class=\"btn btn-primary mt-3\">Volver</a>
+        </div>
     </div>
-
-
-
-
-
-
-    <h1>GRACIES</h1>
-</div>
 ";
         
         $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
@@ -157,7 +156,7 @@ class __TwigTemplate_057d9ab29b34f8f15f1f43ce24514f95db2360a94524630d564cd17bbbc
 
     public function getDebugInfo()
     {
-        return array (  125 => 29,  119 => 26,  113 => 23,  107 => 20,  101 => 17,  88 => 6,  78 => 5,  59 => 3,  36 => 1,);
+        return array (  132 => 34,  126 => 33,  122 => 32,  118 => 31,  114 => 30,  88 => 6,  78 => 5,  59 => 3,  36 => 1,);
     }
 
     public function getSourceContext()
@@ -167,43 +166,39 @@ class __TwigTemplate_057d9ab29b34f8f15f1f43ce24514f95db2360a94524630d564cd17bbbc
 {% block title %}Pago{% endblock %}
 
 {% block body %}
-<style>
-    .example-wrapper { margin: 1em auto; max-width: 800px; width: 95%; font: 18px/1.5 sans-serif; }
-    .example-wrapper code { background: #F5F5F5; padding: 2px 6px; }
-</style>
+    <style>
+        .example-wrapper {
+            margin: 1em auto;
+            max-width: 800px;
+            width: 95%;
+            font: 18px/1.5 sans-serif;
+        }
+        .example-wrapper code {
+            background: #F5F5F5;
+            padding: 2px 6px;
+        }
+    </style>
 
-<div class=\"example-wrapper\">
-    <h1>Pagament y cita correcte</h1>
-    <div class=\"card-body\">
-        <form method=\"post\" class=\"form\" autocomplete=\"off\" role=\"form\">
-
-            <div class=\"form-group\">
-                {{ reserva.talla }}
-            </div>
-            <div class=\"form-group\">
-                {{ reserva.descripcion }}
-            </div>
-            <div class=\"form-group\">
-                {{ reserva.deposito }}
-            </div>
-            <div class=\"form-group\">
-                {{ reserva.cliente.nombre }}
-            </div>
-            <div class=\"form-group\">
-                {{ reserva.fechaInicio |date(\"d/m/Y H\") }}h
-            </div>
-
-        </form>
+    <div class=\"container py-3\">
+    <div class=\"row\">
+    <div class=\"col-md-12\">
+    <div class=\"row justify-content-center\">
+    <div class=\"col-md-6\">
+    <div class=\"card card-outline-secodary\">
+        <div class=\"card-header\">
+            <h3 class=\"mb-0\">Pago y cita correctos</h3>
+        </div>
+        <div class=\"card-body\">
+            <ul class=\"list-group list-group-flush\">
+                <li class=\"list-group-item\"><strong>Talla: </strong><br>{{ reserva.talla }}</li>
+                <li class=\"list-group-item\"><strong>Descripción: </strong><br>{{ reserva.descripcion }}</li>
+                <li class=\"list-group-item\"><strong>Depósito: </strong><br>{{ reserva.deposito }}</li>
+                <li class=\"list-group-item\"><strong>Nombre: </strong><br>{{ reserva.cliente.apellidos }}, {{ reserva.cliente.nombre }}</li>
+                <li class=\"list-group-item\"><strong>Nombre de la Imagen: </strong><br>{{ reserva.imagen }}</li>
+            </ul>
+            <a href=\"/\" role=\"button\" class=\"btn btn-primary mt-3\">Volver</a>
+        </div>
     </div>
-
-
-
-
-
-
-    <h1>GRACIES</h1>
-</div>
-{% endblock %}
-", "pagamiento/success.html.twig", "C:\\Users\\david\\Desktop\\DAW\\M12\\2020-21-DAW2-M12-VLM-TATTOO\\Project\\vlm-tattoo\\templates\\pagamiento\\success.html.twig");
+{% endblock %}", "pagamiento/success.html.twig", "C:\\Users\\david\\Desktop\\DAW\\M12\\2020-21-DAW2-M12-VLM-TATTOO\\Project\\vlm-tattoo\\templates\\pagamiento\\success.html.twig");
     }
 }
